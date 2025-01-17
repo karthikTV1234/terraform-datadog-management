9fc7959b819787687bcba4b942a106c3f2c59ade
karthikTV1234 <karthikmobile1120@gmail.com>
2025-01-17 12:26:41 +0530

    changes for the main

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "datadog" {
+  api_key = "2de1d321d8839fe9aace34f87e248fad"
+  app_key = "e513dd1222affc9e2171efe31034902f0475dad3"
+  api_url = "https://us5.datadoghq.com"
+}
--- versions.tf
+++ versions.tf
@@ -9,7 +9,7 @@ terraform {
   } 
   required_providers {
     datadog = {
-      source  = "DataDog/datadog"
+      source  = "datadog/datadog"
       version = "3.50.0"
     }
     time = {
@@ -19,8 +19,4 @@ terraform {
   }
 }
 
-provider "datadog" {
-  api_key = "2de1d321d8839fe9aace34f87e248fad"
-  app_key = "e513dd1222affc9e2171efe31034902f0475dad3"
-  api_url = "https://us5.datadoghq.com"
-}
+
