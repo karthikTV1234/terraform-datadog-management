ef1b4608464b1cd8adc9d37cf3d5ae2a639c6c99
karthikTV1234 <karthikmobile1120@gmail.com>
2025-01-20 17:34:09 +0530

    Update appkeys.tf

--- appkeys.tf
+++ appkeys.tf
@@ -12,25 +12,3 @@
 #   for_each = toset(var.datadog_api_keys)
 #   name = each.value
 # }
-
-resource "time_rotating" "api_keys" {
-  # for_each         = toset(var.datadog_api_keys)
-  for_each =  {for key in var.datadog_api_keys:key.name=>key}
-  rotation_minutes = 2
-
-  triggers = {
-    rotation_trigger = each.key  
-  }
-}
-
-resource "datadog_api_key" "api_keys" {
- for_each =  {for key in var.datadog_api_keys:key.name=>key}
-
-  name = each.value.name
-
-  lifecycle {
-    create_before_destroy = true 
-  }
-
-  depends_on = [time_rotating.api_keys]  
-}
