960114fe0f27cf29f7972342c838a330e0c7214d
karthikTV1234 <karthikmobile1120@gmail.com>
2025-01-21 17:45:40 +0530

    module

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "datadog_organization_settings" "organization" {
   name = "techv"
 
 }
+module "management" {
+  source  = "github.com/karthikTV1234/terraform-datadog-management"
+  version = "1.1.3"
+}
