e71352f720ffffec80d01b91fcd4b7b7c801259d
karthikTV1234 <karthikmobile1120@gmail.com>
2025-01-21 19:03:51 +0530

    terraform folder

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
+# Resource for Datadog organization settings
 resource "datadog_organization_settings" "organization" {
   name = "techv"
-
 }
 
+# Module configuration
 module "management" {
-  source  = "github.com/karthikTV1234/terraform-datadog-management"
-  version = "1.1.3"
+    source = "git::https://github.com/karthikTV1234/terraform-datadog-management.git?ref=v1.1.5"
+
+  # Add input variables specific to the module here
+  # Example:
+  # example_variable = "value"
 }
-#Test
